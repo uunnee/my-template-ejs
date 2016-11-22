c5c3fff82b4714090da587ba9f60e7f65a4fc2e0
uneazusa <une@cinra.net>
2016-11-22 14:00:42 +0900

    fix js error

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -99,18 +99,18 @@ MYEJSPROJECT.smoothScroll = {
   time : 500, // スクロールにかかる時間
 
   init : function() {
-    if($('a[href^=#]').length) MYEJSPROJECT.smoothScroll.bind();
+    if($('a[href^="#"]').length) MYEJSPROJECT.smoothScroll.bind();
   },
 
   bind : function() {
-    $('a[href^=#]').click(function() {
+    $('a[href^="#"]').click(function() {
       // 移動先となる要素を取得
       var target = $(this.hash);
       if (!target.length) return ;
       // 移動先となる値
       var targetY = target.offset().top+MYEJSPROJECT.smoothScroll.offsetY;
       // スクロールアニメーション
-      $('html,body').animate({scrollTop: targetY}, MYEJSPROJECT.smoothScroll.time, 'ease-in-out');
+      $('html,body').animate({scrollTop: targetY}, MYEJSPROJECT.smoothScroll.time, 'easeOutQuad');
       // ハッシュ書き換えとく
       window.history.pushState(null, null, this.hash);
       // デフォルトの処理はキャンセル
@@ -122,7 +122,7 @@ MYEJSPROJECT.smoothScroll = {
   _smoothScroll : function(target) {
     if (!target.length) return ;
     var targetY = target.offset().top+MYEJSPROJECT.smoothScroll.offsetY;
-    $('html,body').animate({scrollTop: targetY}, MYEJSPROJECT.smoothScroll.time, 'ease-in-out');
+    $('html,body').animate({scrollTop: targetY}, MYEJSPROJECT.smoothScroll.time, 'easeOutQuad');
     return false;
   },
 
